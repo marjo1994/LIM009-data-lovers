5df72d7b8662bb0e88eb3fc808d6fdd3ca53103f
Cindy Mendoza <mendozaic@hotmail.com>
2019-03-20 15:45:56 -1100

    Obteniendo tipos de pokemones-Code Review Lulu

--- src/data.js
+++ src/data.js
@@ -1,16 +1,3 @@
-/* Manejo de data */
-
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
-
-/*const example = () => {
-  return 'example';
-};
-
-window.example = example;*/
-
-//Ordenar por N°Apariciones
-
 
 const sorData =(dataPokemon, sortBy, sortOrder) =>{
 let pokemonOrder =[];
@@ -26,34 +13,27 @@ break;
 
 case 'DESC':
 if (sortBy==='name'){
-  pokemonOrder =dataPokemon.sort((a, b) => (a.name < b.name ? 1 : -1))
+  pokemonOrder =dataPokemon.sort((a, b) => (a.name < b.name ? 1 : -1));
 }else{
-  pokemonOrder =dataPokemon.sort((a, b) => (a.avg_spawns < b.avg_spawns ? 1 : -1))
+  pokemonOrder =dataPokemon.sort((a, b) => (a.avg_spawns < b.avg_spawns ? 1 : -1));
 }
 break;
 default:
 }
 return pokemonOrder;
+};
+
+window.sorData = sorData;
+
+const selectUniqueTypes = (data) => {
+  let tipos =[];
+  data.map((obj) => {
+  obj.type.forEach((string)=>{
+    tipos.push(string);
+  });
+});
+const distintos =[...new Set(tipos)];
+  return distintos
 }
 
-window.sorData =sorData;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+window.selectUniqueTypes = selectUniqueTypes;
--- src/main.js
+++ src/main.js
@@ -1,27 +1,31 @@
 /* Manejo del DOM */
 const dataPokemon = window.POKEMON.pokemon;
 //Mostrar data en pantalla
-let displayCards = () => {
-        return document.getElementById("cards").innerHTML =
-        `${dataPokemon.map( (dataPokemon) => {
-            return `Nombre: ${dataPokemon.name}
-            <br> Altura: ${dataPokemon.height}
-            <br> Peso: ${dataPokemon.weight}
-            <br> N°apariciones/10,000 : ${dataPokemon.avg_spawns} veces
-            <br> Hora probable para encontrarlo: ${dataPokemon.spawn_time}
-            <img src ="${dataPokemon.img}"/>
-            <br><br>
-            `;
-        }).join('')}`;
-    };
-       
+const cardsContainer = document.getElementById("cards");
+
+const displayCards = (data) => {
+    let string = '';
+
+    for(let i = 0; i <data.length; i++){
+       string +=  `Nombre: ${data[i].name}
+       <br> Altura: ${data[i].height}
+       <br> Peso: ${data[i].weight}
+       <br> N°apariciones/10,000 : ${data[i].avg_spawns} veces
+       <br> Hora probable para encontrarlo: ${data[i].spawn_time}
+       <br> Tipos: ${data[i].type}
+       <br> <img src ="${data[i].img}"/>
+       <br><br>`
+    }
+
+    return cardsContainer.innerHTML = string;
+}
+
 displayCards(dataPokemon);
 
 //Mostrar en pantalla Cards ordenadas por nombre
 document.getElementById("ordsnameA").addEventListener("click",() => {
     displayCards(sorData(dataPokemon,'name','ASC'))
     });
-
 
 document.getElementById("ordsnameD").addEventListener("click",()=>{
     displayCards(sorData(dataPokemon,'name','DESC'))
@@ -35,13 +39,21 @@ document.getElementById("ordspawnA").addEventListener("click",() => {
     displayCards(sorData(dataPokemon,'avg_spawns','ASC'))
     });
 
-
 document.getElementById("ordspawnD").addEventListener("click",()=>{
     displayCards(sorData(dataPokemon,'avg_spawns','DESC'))
 });
 
+//Filtrar por tipo de pokemon
+console.log(selectUniqueTypes(dataPokemon));
 
-
-
-
-
+const displaySelectTypesOfPokemon = (types) => {
+    let string = '';
+    for(let i = 0; i < types.length; i++){
+        string += ` <select>
+        <option value="volvo"> ${types[i]}Volvo</option>
+        <option value="saab">Saab</option>
+        <option value="vw">VW</option>
+        <option value="audi" selected>Audi</option>
+      </select> `
+    }
+}
